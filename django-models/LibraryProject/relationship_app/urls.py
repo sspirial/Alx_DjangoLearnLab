cf5937283e9087a70dad68831b93be10dee4d414
Munubi Emmanuel <sspirial@gmail.com>
2025-09-20 07:33:46 +0300

    Refactor authentication routes in urls.py for improved readability

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,18 +9,7 @@ urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     # Authentication routes
-    path(
-        "login/",
-        LoginView.as_view(
-            template_name="relationship_app/login.html",
-            redirect_authenticated_user=True,
-        ),
-        name="login",
-    ),
-    path(
-        "logout/",
-        LogoutView.as_view(template_name="relationship_app/logout.html"),
-        name="logout",
-    ),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html",redirect_authenticated_user=True), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
